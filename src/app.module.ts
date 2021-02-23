4d4601982906273c98656c14c648c58343a1251a
Jorge Cesar <cdialike@gmail.com>
2021-02-23 23:09:39 +0100

    docker changed conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/bnext'),
+    MongooseModule.forRoot('mongodb://localhost:27017/bnext'),
     UserModule
   ],
   controllers: [AppController],
